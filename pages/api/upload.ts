cfa167343269a8f84f50bd240eabd589d41ab89b
matgu <87834682+mgonline2021@users.noreply.github.com>
2024-04-17 05:28:23 +0200

    Update and rename upload.js to upload.ts

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,23 +1,28 @@
-// pages/api/upload.js
+// pages/api/upload.ts
 import nextConnect from 'next-connect';
 import multer from 'multer';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+interface ExtendedNextApiRequest extends NextApiRequest {
+  file: any;  // Aggiungi una definizione di tipo più specifica se necessario
+}
 
 const upload = multer({
   storage: multer.memoryStorage(),
 });
 
 const apiRoute = nextConnect({
-  onError(error, req, res) {
+  onError(error: Error, req: ExtendedNextApiRequest, res: NextApiResponse) {
     res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
   },
-  onNoMatch(req, res) {
+  onNoMatch(req: ExtendedNextApiRequest, res: NextApiResponse) {
     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
   },
 });
 
 apiRoute.use(upload.single('file'));
 
-apiRoute.post((req, res) => {
+apiRoute.post((req: ExtendedNextApiRequest, res: NextApiResponse) => {
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
